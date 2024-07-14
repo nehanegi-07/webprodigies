0161f4e4587645f559ecc2ea0d8e720e0d78e534
Neha <nehanegi071097@gmail.com>
2024-07-14 13:41:50 +0530

    second commit

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/nehanegi/Desktop/webprodigies-plura/src/app/layout\":[\"static/media/7e6a2e30184bb114-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
